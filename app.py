4ef7fa04ec29d687304624cb950ff782129fdf2e
Harsayazheni <harsayazheni@gmail.com>
2024-09-09 19:42:52 +0530

    deployment

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 # Temporary storage for users
 users = {}
 
-@app.route('/')
+@app.route('home')
 def index():
     return render_template('home.html')
 
-@app.route('/register')
+@app.route('register')
 def register_page():
     return render_template('register.html')
 
-@app.route('/register', methods=['POST'])
+@app.route('register', methods=['POST'])
 def register():
     data = request.get_json()
     username = data.get('username')
@@ -27,7 +27,7 @@ def register():
     users[username] = {'email': email, 'password': password}
     return jsonify({'message': 'Registration successful'}), 200
 
-@app.route('/login', methods=['POST'])
+@app.route('login', methods=['POST'])
 def login():
     data = request.get_json()
     username = data.get('username')
